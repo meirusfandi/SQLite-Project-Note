8979585784dcfca11eee1ce0e69d7eeff886c7b9
meirusfandi <meirusfandi100@gmail.com>
2018-09-16 22:20:03 +0700

    version 1.0.0

---
+++ app/src/main/java/com/example/root/sqliteproject/DatabaseContract.java
@@ -0,0 +1,1 @@
+package com.example.root.sqliteproject;import android.provider.BaseColumns;public class DatabaseContract {    static String TABLE_NOTE    = "note";    static final class NoteColumns implements BaseColumns {        static String TITLE         = "title";        static String DESCRIPTION   = "description";        static String DATE          = "date";    }}
